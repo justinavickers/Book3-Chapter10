af23d53c056bd70b42d0716ff4725c20838e4720
justinavickers <justinav38@gmail.com>
2019-02-07 21:34:55 -0600

    posting to DOM

--- ContactList/browserify-boilerplate/src/scripts/contact.js
+++ ContactList/browserify-boilerplate/src/scripts/contact.js
@@ -1,7 +1,7 @@
 //boilerplate that is going to list all contacts
 function contactDiv(contact) {
   let contacts=
-    `<div class="contact>
+    `<div class="contact">
     <h1> ${contact.name}</h1>
     <p> ${contact.phoneNumber}</p>
     <p> ${contact.address}</p>
--- ContactList/browserify-boilerplate/src/scripts/main.js
+++ ContactList/browserify-boilerplate/src/scripts/main.js
@@ -1,6 +1,5 @@
 import createInformation from "./contactForm"
 import contactListToDom from "./contactList"
-import renderContacts from "./contactToDOM";
 //things we need to pull and inject into the ^^
 console.log("hello")
 //just checking to make sure we can see the JS main^^
